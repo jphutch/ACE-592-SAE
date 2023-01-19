e877c82bde16a0a0f99413a394071ba687bf05c3
Jared Hutchins <10506076+jphutch@users.noreply.github.com>
2023-01-19 16:37:19 -0600

    A list of shell commands demonstrating sed and grep

---
+++ Lectures/1_Introduction/22_shell_commands.sh
@@ -0,0 +1,18 @@
+grep "rain" song_lyrics.txt
+
+grep "storm" song_lyrics.txt
+
+grep -n "storm" song_lyrics.txt
+
+grep -A 3 "storm" song_lyrics.txt
+
+grep -A 3 "storm" song_lyrics.txt > second_chorus.txt
+
+#  -i : do this in place
+#  s/ : do a substitution
+#  s/regex replace/ what to replace it with/g
+# /g: do this globally, so dont just do the first one.
+
+sed -i 's/Adele/Bob Dylan/g' song_lyrics.txt
+sed -i 's/19/Time Out of Mind/g' song_lyrics.txt
+sed -i 's/Rolling in the deep/To make you feel my love/g' song_lyrics.txt
