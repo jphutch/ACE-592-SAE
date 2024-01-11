3a4b50fdd2edba569389f6442ce0a653389cb218
Jared Hutchins <10506076+jphutch@users.noreply.github.com>
2024-01-11 10:06:11 -0600

    Update python notebooks for intro module

--- Lectures/1_Introduction/22_shell_commands.sh
+++ Lectures/1_Introduction/22_shell_commands.sh
@@ -15,4 +15,4 @@ grep -A 3 "storm" song_lyrics.txt > second_chorus.txt
 
 sed -i 's/Adele/Bob Dylan/g' song_lyrics.txt
 sed -i 's/19/Time Out of Mind/g' song_lyrics.txt
-sed -i 's/Rolling in the deep/To make you feel my love/g' song_lyrics.txt
+sed -i 's/Rolling in the deep/To make you feel my love' song_lyrics.txt
